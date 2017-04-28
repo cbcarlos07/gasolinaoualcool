2a8ff475f399c429c7ad046b6162cbf461677e6a
Carlos Bruno <cbcarlos08@gmail.com>
2017-04-28 14:58:18 -0400

    Gasolina ou Alcool

--- app/src/main/java/gasolinaoualcool/cursoandroid/com/gasolinaoualcool/MainActivity.java
+++ app/src/main/java/gasolinaoualcool/cursoandroid/com/gasolinaoualcool/MainActivity.java
@@ -2,12 +2,49 @@ package gasolinaoualcool.cursoandroid.com.gasolinaoualcool;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+     private EditText precoAlcool;
+     private EditText precoGasolina;
+     private Button   botaoVerificar;
+     private TextView textoResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        precoAlcool = (EditText) findViewById(R.id.precoAlcoolId);
+        precoGasolina = (EditText) findViewById(R.id.precoGasolinaId);
+        botaoVerificar = (Button) findViewById(R.id.botaoVerificarId);
+        textoResultado = (TextView) findViewById(R.id.textoResultadoId);
+
+        botaoVerificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //recupera valores digitados
+                String textoPrecoAlcool = precoAlcool.getText().toString();
+                String textoPrecoGasolina = precoGasolina.getText().toString();
+
+                //conveter valores Strings para numeros
+                Double valorAlcool = Double.parseDouble(textoPrecoAlcool);
+                Double valorGasolina = Double.parseDouble(textoPrecoGasolina);
+
+                //alcool / gasolina
+                double resultado = valorAlcool / valorGasolina;
+                if(resultado >= 0.7){
+                    //gasolina
+                    textoResultado.setText("É melhor utilizar a gasolina");
+                }else{
+                    //alcool
+                    textoResultado.setText("É melhor utilizar o álcool");
+                }
+
+            }
+        });
+
     }
 }
